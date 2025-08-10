653bb3734d6e65be697758b11d1451414c2c73ac
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2025-08-10 23:11:05 +0530

    Local mysql database connected

--- src/main/java/com/learning/jpa/JPA/entities/Product.java
+++ src/main/java/com/learning/jpa/JPA/entities/Product.java
@@ -1,13 +1,12 @@
 package com.learning.jpa.JPA.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -17,15 +16,30 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 @Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(name = "title_price_unique", columnNames = {"title", "price"}) // columnName has the same name as property
+        },
+        indexes = {
+                @Index(name = "sku_index", columnList = "sku")
+        }
+)
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 20)
     private String sku;
+
     private String title;
     private BigDecimal price;
     private Integer quantity;
+
+    @CreationTimestamp
     private LocalDate createdAt;
+
+    @UpdateTimestamp
     private LocalDate updatedAt;
 
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into test.product (price, quantity, sku, title)
+values
+(12.4, 4, 'parle734', 'Parle Buscuit'),
+(14.4, 1, 'pepsi123', "Pepsi"),
+(34.4, 10, 'Coke16', 'Coca Cola'),
+(16.4, 3, 'pepsi156', "Mazza");
